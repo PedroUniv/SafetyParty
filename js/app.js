deb20c9a3e4c63748013ed2f7cc9aa8077b89a29
PedroUniv <267714615+PedroUniv@users.noreply.github.com>
2026-05-20 03:15:09 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ particlesJS("particles-js", {
     modes: {
       grab: { distance: 400, line_linked: { opacity: 1 } },
       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 135.86413586413587, duration: 0.4 },
+      repulse: { distance: 120, duration: 0.4 },
       push: { particles_nb: 4 },
       remove: { particles_nb: 2 }
     }
