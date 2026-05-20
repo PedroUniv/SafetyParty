a3dfd3865052c41eed2b2bc2b1fe0e5f8d6652db
PedroUniv <267714615+PedroUniv@users.noreply.github.com>
2026-05-20 02:51:32 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,41 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles-js config loaded');
+});
+
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 50
+    },
+
+    color: {
+      value: "#8cff00"
+    },
+
+    shape: {
+      type: "circle"
+    },
+
+    opacity: {
+      value: 0.4
+    },
+
+    size: {
+      value: 2
+    },
+
+    line_linked: {
+      enable: true,
+      distance: 120,
+      color: "#8cff00",
+      opacity: 0.2,
+      width: 1
+    },
+
+    move: {
+      enable: true,
+      speed: 2
+    }
+  }
+});
