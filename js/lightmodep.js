fb51cc7eb39b4b91a0611a563ed4d746f69cb76a
PedroUniv <267714615+PedroUniv@users.noreply.github.com>
2026-05-22 19:51:10 -0300

    Adicionado modo claro ao site

--- js/lightmodep.js
+++ js/lightmodep.js
@@ -1,15 +1,37 @@
 function lightmodep() {
-            const elemento = document.getElementById('particles-js') || document.getElementById('light-mode-particles');
-            
-            // Alterna entre os IDs
-            if (elemento.id === 'particles-js') {
-                elemento.id = 'light-mode-particles';
-            } else {
-                elemento.id = 'particles-js';
-            }
-            
-            console.log('ID atual do elemento:', elemento.id);
 
-            localStorage.setItem('tema', 'light'); 
+    const elemento =
+        document.getElementById('particles-js') ||
+        document.getElementById('light-mode-particles');
+
+    // Alterna entre os IDs
+    if (elemento.id === 'particles-js') {
+
+        // ATIVA modo claro
+        elemento.id = 'light-mode-particles';
+
+        // Texto preto
+        document.body.style.color = 'black';
+
+        // Fundo claro
+        document.body.style.backgroundColor = 'white';
+
+        localStorage.setItem('tema', 'light');
+
+    } else {
+
+        // VOLTA modo escuro
+        elemento.id = 'particles-js';
+
+        // Texto branco
+        document.body.style.color = 'white';
+
+        // Fundo escuro
+        document.body.style.backgroundColor = '#111';
+
+        localStorage.setItem('tema', 'dark');
+    }
+
+    console.log('ID atual do elemento:', elemento.id);
+    
 }
-
